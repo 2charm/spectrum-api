81c17304b64e8c3dd314d617ac6802f0e3a7ea59
Andrew Hwang <andrewhwang10@gmail.com>
2019-04-28 16:55:17 -0700

    Add Redis and SQL server drivers and routes for user and sessions

--- gateway/main.go
+++ gateway/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/2charm/spectrum-api/gateway/handlers"
+	"github.com/2charm/spectrum-api/gateway/models/sessions"
+	"github.com/2charm/spectrum-api/gateway/models/users"
+	"github.com/go-redis/redis"
 )
 
 func failOnError(err error, msg string) {
@@ -24,14 +29,46 @@ func getEnvironmentVariable(key string) string {
 
 func main() {
 	addr := getEnvironmentVariable("ADDR")
-	key := getEnvironmentVariable("KEY")
+	apikey := getEnvironmentVariable("APIKEY")
 	tlscert := getEnvironmentVariable("TLSCERT")
 	tlskey := getEnvironmentVariable("TLSKEY")
+	sessionkey := getEnvironmentVariable("SESSIONKEY")
+	redisaddr := getEnvironmentVariable("REDISADDR")
+	routingkey := getEnvironmentVariable("ROUTINGKEY")
+	dsn := getEnvironmentVariable("DSN")
 
-	ctx := handlers.HandlerContext{key}
+	//Redis Server
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisaddr,
+		Password: "",
+		DB:       0,
+	})
+	_, err := rdb.Ping().Result()
+	failOnError(err, "Error pinging redis database")
+
+	rs := sessions.NewRedisStore(rdb, 150*time.Second)
+
+	//mySQL Server
+	db, err := sql.Open("mysql", dsn)
+	failOnError(err, "Error opening a new SQL database")
+
+	err = db.Ping()
+	failOnError(err, "Error pinging database")
+	log.Printf("Successfully connected to SQL database!\n")
+
+	ms := users.NewMySQLStore(db)
+
+	ctx := handlers.HandlerContext{
+		SigningKey:   sessionkey,
+		SessionStore: rs,
+		UserStore:    ms,
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/news", ctx.NewsHandler)
+	mux.HandleFunc("/v1/news", ctx.NewsHandler)                //Get news
+	mux.HandleFunc("/v1/users", ctx.UsersHandler)              //Create user
+	mux.HandleFunc("/v1/sessions", ctx.SessionsHandler)        //Login user
+	mux.HandleFunc("v1/sessions/", ctx.SpecificSessionHandler) //Logout user
 	wrappedMux := handlers.NewResponseHeader(mux)
 	log.Printf("server is listening at %s...", addr)
 	log.Fatal(http.ListenAndServeTLS(addr, tlscert, tlskey, wrappedMux))
--- gateway/update.sh
+++ gateway/update.sh
@@ -2,20 +2,39 @@
 docker login
 docker volume prune -f
 docker rm -f gateway
+docker rm -f redis_server
+docker rm -f sql_server
 docker network rm service_network
 docker network create service_network
+
 #Set env variables
 export ADDR=:443
+export REDISADDR="redis_server:6379"
+export APIKEY="`cat ./news_api.key`"
+export SESSIONKEY="keykey"
+export SQLADDR=3306
+export MYSQL_ROOT_PASSWORD="sqlkey"
+export DSN="root:$MYSQL_ROOT_PASSWORD@tcp(sql_server:$SQLADDR)/mysql"
 export TLSCERT="/etc/letsencrypt/live/api.spectrumnews.me/fullchain.pem"
 export TLSKEY="/etc/letsencrypt/live/api.spectrumnews.me/privkey.pem"
-export KEY="`cat ./news_api.key`"
+
+#Redis Server
+docker run -d --network service_network --name redis_server redis
+#SQL Server
+docker pull 2charm/sql
+docker run -d --network service_network --name sql_server \
+-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+-e MYSQL_DATABASE=mysql \
+2charm/sql
 
 docker pull 2charm/gateway
 docker run -d --network service_network --name gateway \
 -p 443:443 \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
--e ADDR=$ADDR \
--e KEY=$KEY \
 -e TLSCERT=$TLSCERT \
 -e TLSKEY=$TLSKEY \
+-e ADDR=$ADDR \
+-e APIKEY=$APIKEY \
+-e SESSIONKEY=$SESSIONKEY \
+-e DSN=$DSN \
 2charm/gateway
