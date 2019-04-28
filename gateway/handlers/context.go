a3c1c9dfa0940d52eef83e06f80bc2b1d3460907
Andrew Hwang <andrewhwang10@gmail.com>
2019-04-28 16:26:36 -0700

    Update HandlerContext to have User and Session stores

--- gateway/handlers/context.go
+++ gateway/handlers/context.go
@@ -1,5 +1,14 @@
 package handlers
 
+import (
+	"github.com/2charm/spectrum-api/gateway/models/sessions"
+	"github.com/2charm/spectrum-api/gateway/models/users"
+)
+
+//HandlerContext represents a receiver for handlers to utilize and gain context
 type HandlerContext struct {
-	Key string
+	APIKey       string
+	SigningKey   string
+	SessionStore sessions.Store `json:"sessionStore,omitempty"`
+	UserStore    users.Store    `json:"userStore,omitempty"`
 }
