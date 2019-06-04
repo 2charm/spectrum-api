a6230ef160c3cef0ed201b76d871a971995565ad
Andrew Hwang <andrewhwang10@gmail.com>
2019-06-03 17:00:13 -0700

    Organize repository code in to seperate packages

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"time"
 
 	"github.com/2charm/spectrum-api/pkg/util"
@@ -16,6 +19,7 @@ import (
 
 func main() {
 	addr := util.GetEnvironmentVariable("ADDR")
+	newsaddr := util.GetEnvironmentVariable("NEWSADDR")
 	apikey := util.GetEnvironmentVariable("APIKEY")
 	tlscert := util.GetEnvironmentVariable("TLSCERT")
 	tlskey := util.GetEnvironmentVariable("TLSKEY")
@@ -51,12 +55,33 @@ func main() {
 		UserStore:    ms,
 	}
 
+	newsURL, err := url.Parse("http://" + newsaddr)
+	util.FailOnError(err, "Invalid URL for microservice")
+	newsProxy := &httputil.ReverseProxy{Director: customDirector(newsURL, &ctx)}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/news", ctx.NewsHandler)                //Get news
+	mux.Handle("/v1/news", newsProxy)                          //Get news
 	mux.HandleFunc("/v1/users", ctx.UsersHandler)              //Create user
 	mux.HandleFunc("/v1/sessions", ctx.SessionsHandler)        //Login user
 	mux.HandleFunc("v1/sessions/", ctx.SpecificSessionHandler) //Logout user
 	wrappedMux := handlers.NewResponseHeader(mux)
 	log.Printf("server is listening at %s...", addr)
 	log.Fatal(http.ListenAndServeTLS(addr, tlscert, tlskey, wrappedMux))
+}
+
+func customDirector(target *url.URL, ctx *handlers.HandlerContext) func(*http.Request) {
+	return func(r *http.Request) {
+		sessState := handlers.SessionState{}
+		_, err := sessions.GetState(r, ctx.SigningKey, ctx.SessionStore, sessState)
+		if err == nil {
+			obj, err := json.Marshal(sessState.User)
+			if err == nil {
+				r.Header.Del("X-User")
+				r.Header.Add("X-User", string(obj))
+			}
+		}
+		r.Host = target.Host
+		r.URL.Host = target.Host
+		r.URL.Scheme = target.Scheme
+	}
 }
--- cmd/news/main.go
+++ cmd/news/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "github.com/2charm/spectrum-api/pkg/util"
+import (
+	"log"
+	"net/http"
+
+	"github.com/2charm/spectrum-api/pkg/news"
+	"github.com/2charm/spectrum-api/pkg/util"
+)
 
 func main() {
 	addr := util.GetEnvironmentVariable("ADDR")
 	apikey := util.GetEnvironmentVariable("APIKEY")
+	dsn := util.GetEnvironmentVariable("DSN")
+
+	//mySQL Server
+	db, err := sql.Open("mysql", dsn)
+	util.FailOnError(err, "Error opening a new SQL database")
+
+	err = db.Ping()
+	util.FailOnError(err, "Error pinging database")
+	log.Printf("Successfully connected to SQL database!\n")
+
+	ctx := news.HandlerContext{
+		APIKey: apikey,
+		NewsStore: 
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/news", ctx.NewsHandler)         //Get news
+	mux.HandleFunc("/v1/spectrum", ctx.SpectrumHandler) //Get full spectrum of news
+
+	log.Printf("server is listening at %s...", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
