8752ae8e377217fdeec8baf3e456c5e730f6c07a
Andrew Hwang <andrewhwang10@gmail.com>
2019-06-03 17:00:32 -0700

    Update sql database definition to have articles

--- database/schema.sql
+++ database/schema.sql
@@ -15,11 +15,31 @@ create table if not exists sign_in (
 
 create table if not exists categories (
     category_id int not null auto_increment primary key,
-    category_name varchar(128) not null unique,
+    category_name varchar(128) not null unique
 );
+
+insert into categories
+values(1, 'sports');
+insert into categories
+values(2, 'health');
+insert into categories
+values(3, 'business');
+insert into categories
+values(4, 'entertainment');
+insert into categories
+values(5, 'science');
+insert into categories
+values(6, 'technology');
+insert into categories
+values(7, 'headline');
 
 create table if not exists articles (
     user_id int not null,
     category_id int not null,
     read_on datetime not null
-)
+);
+
+create table if not exists sources (
+    source_id int not null auto_increment primary key,
+    source_name varchar(128) not null unique
+);
