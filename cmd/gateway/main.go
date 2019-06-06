660c330e98f6370f98f04e3cdaed5a075fc7dcdb
Andrew Hwang <andrewhwang10@gmail.com>
2019-06-06 14:44:42 -0700

    Create route from gateway to news microservice

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -20,7 +20,6 @@ import (
 func main() {
 	addr := util.GetEnvironmentVariable("ADDR")
 	newsaddr := util.GetEnvironmentVariable("NEWSADDR")
-	apikey := util.GetEnvironmentVariable("APIKEY")
 	tlscert := util.GetEnvironmentVariable("TLSCERT")
 	tlskey := util.GetEnvironmentVariable("TLSKEY")
 	sessionkey := util.GetEnvironmentVariable("SESSIONKEY")
@@ -36,7 +35,7 @@ func main() {
 	_, err := rdb.Ping().Result()
 	util.FailOnError(err, "Error pinging redis database")
 
-	rs := sessions.NewRedisStore(rdb, 150*time.Second)
+	rs := sessions.NewRedisStore(rdb, time.Hour)
 
 	//mySQL Server
 	db, err := sql.Open("mysql", dsn)
@@ -49,7 +48,6 @@ func main() {
 	ms := users.NewMySQLStore(db)
 
 	ctx := handlers.HandlerContext{
-		APIKey:       apikey,
 		SigningKey:   sessionkey,
 		SessionStore: rs,
 		UserStore:    ms,
@@ -57,13 +55,17 @@ func main() {
 
 	newsURL, err := url.Parse("http://" + newsaddr)
 	util.FailOnError(err, "Invalid URL for microservice")
+
+	log.Printf("News Microservice URL: %s", newsURL.String())
 	newsProxy := &httputil.ReverseProxy{Director: customDirector(newsURL, &ctx)}
 
 	mux := http.NewServeMux()
-	mux.Handle("/v1/news", newsProxy)                          //Get news
-	mux.HandleFunc("/v1/users", ctx.UsersHandler)              //Create user
-	mux.HandleFunc("/v1/sessions", ctx.SessionsHandler)        //Login user
-	mux.HandleFunc("v1/sessions/", ctx.SpecificSessionHandler) //Logout user
+	mux.Handle("/v1/news", newsProxy)                           //Get news
+	mux.Handle("/v1/spectrum/", newsProxy)                      //Get related news
+	mux.Handle("/v1/metrics", newsProxy)                        //Get and post metrics
+	mux.HandleFunc("/v1/users", ctx.UsersHandler)               //Create user
+	mux.HandleFunc("/v1/sessions", ctx.SessionsHandler)         //Login user
+	mux.HandleFunc("/v1/sessions/", ctx.SpecificSessionHandler) //Logout user
 	wrappedMux := handlers.NewResponseHeader(mux)
 	log.Printf("server is listening at %s...", addr)
 	log.Fatal(http.ListenAndServeTLS(addr, tlscert, tlskey, wrappedMux))
@@ -71,14 +73,18 @@ func main() {
 
 func customDirector(target *url.URL, ctx *handlers.HandlerContext) func(*http.Request) {
 	return func(r *http.Request) {
-		sessState := handlers.SessionState{}
+		sessState := &handlers.SessionState{}
+		log.Print("Accessing session...")
 		_, err := sessions.GetState(r, ctx.SigningKey, ctx.SessionStore, sessState)
 		if err == nil {
 			obj, err := json.Marshal(sessState.User)
 			if err == nil {
+				log.Print("Valid User!")
 				r.Header.Del("X-User")
 				r.Header.Add("X-User", string(obj))
 			}
+		} else {
+			log.Printf("Error getting state: %v", err)
 		}
 		r.Host = target.Host
 		r.URL.Host = target.Host
--- cmd/news/main.go
+++ cmd/news/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"time"
+
+	cache "github.com/patrickmn/go-cache"
 
 	"github.com/2charm/spectrum-api/pkg/news"
 	"github.com/2charm/spectrum-api/pkg/util"
@@ -16,19 +20,22 @@ func main() {
 	//mySQL Server
 	db, err := sql.Open("mysql", dsn)
 	util.FailOnError(err, "Error opening a new SQL database")
-
 	err = db.Ping()
 	util.FailOnError(err, "Error pinging database")
 	log.Printf("Successfully connected to SQL database!\n")
 
+	as := news.NewArticleStore(db)
+
 	ctx := news.HandlerContext{
-		APIKey: apikey,
-		NewsStore: 
+		APIKey:       apikey,
+		ArticleStore: as,
+		ArticleCache: cache.New(time.Minute*15, time.Minute*25),
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/news", ctx.NewsHandler)         //Get news
-	mux.HandleFunc("/v1/spectrum", ctx.SpectrumHandler) //Get full spectrum of news
+	mux.HandleFunc("/v1/news", ctx.NewsHandler)          //Get news
+	mux.HandleFunc("/v1/spectrum/", ctx.SpectrumHandler) //Get full spectrum of news
+	mux.HandleFunc("/v1/metrics", ctx.MetricsHandler)    //Get and post metrics
 
 	log.Printf("server is listening at %s...", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
